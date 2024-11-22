5cbbb4eadb8e6ecd84fd4cdbc0ca60c014c2beab
surajgoraicse <surajgoraicse@gmail.com>
2024-11-22 18:42:18 +0530

    lecture 1 completed

--- lecture2/index.js
+++ lecture2/index.js
@@ -1,15 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+// const port = 3000
+const PORT = process.env.PORT;
+console.log(PORT);
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Welcome to backend')
+  res.send('Hello World! ')
 })
 
 app.get('/api/data', (req, res) => {
     res.send("Your data is here")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
