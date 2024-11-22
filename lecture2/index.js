de8fe5b2aaa37cec7ddcb70f7db56c0f5174c6b8
surajgoraicse <surajgoraicse@gmail.com>
2024-11-22 18:30:15 +0530

    initiated a backend project

---
+++ lecture2/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World! Welcome to backend')
+})
+
+app.get('/api/data', (req, res) => {
+    res.send("Your data is here")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
